367a2bb6ed99838d4e199c0a60507411da5ae9cc
ahmad <mkvlogs174@gmail.com>
2024-11-21 18:33:37 +0500

    language update

--- i18n.js
+++ i18n.js
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import en from "./locales/en.json"; // Your English translations
-import es from "./locales/es.json"; // Your Spanish translations
+import { en } from "./locales/en.json"; // Your English translations
+import { es } from "./locales/es.json"; // Your Spanish translations
 
 i18n.use(initReactI18next).init({
   resources: {
